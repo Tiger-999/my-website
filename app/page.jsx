a6c4d7196c5784b84aa9fb010ab4e2fd01babc19
Tiger-999 <finalking07@gmail.com>
2025-07-11 19:27:03 +0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    // <div className="min-h-screen bg-gradient-to-br from-pink-500 to-yellow-400 flex items-center justify-center">
+      <div>
+      {/* <h1 className="text-4xl font-bold text-white"> */}
+      <h1>
+        Welcome to my website
+      </h1>
+      <h2>My name is Nutdanai Panyawisetpongse.</h2>
+      <h2>My nick name is Tiger.</h2>
+      <p>My tech stack</p>
+      <p>Frontend: HTML, CSS, JavaScript, React.js, Bootstrap</p>
+      <p>Backend: Node.js, Express.js, RESTful APIs</p>
+      <p>Database: PostgreSQL, MySQL</p>
+      <p>Tools: Postman, GitHub, Visual Studio Code</p>
+      <p>Programming Languages: Javascript, Typescript, Python, C</p>
+      <p>Github: https://github.com/Tiger-999</p>
+      
+      </div>
+    // {/* </div> */}
+  );
+}
